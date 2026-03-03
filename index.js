6c58b0ddc4084825b0afa0d2ed5d49b9bc878630
JossRegis <jonathanbidou@sfr.fr>
2026-03-03 22:31:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -422,5 +422,15 @@ cron.schedule("55 22 * * 0", async () => {
   });
 
 });
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Bot is running!");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Web server running");
+});
 
 client.login(TOKEN);
